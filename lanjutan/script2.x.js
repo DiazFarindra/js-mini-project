4bd17b05edef3d82434df5c7deddc09bb490e6c8
diaz farindra <farindrad@gmail.com>
2021-10-23 13:39:32 +0700

    update file

---
+++ lanjutan/script2.x.js
@@ -0,0 +1,32 @@
+// EXECUTION CONTEXT, HOISTING & SCOPE
+
+// var nama = 'diaz'
+// console.log(nama);
+
+// creation phase pada global context
+// nama var = undifined
+// nama function = fn()
+// hoisting
+// window = global object
+// this = window
+
+// execution phase
+
+
+// var nama = 'diaz'
+// var umur = 19
+
+// console.log(sayHello())
+
+// function sayHello() {
+//     return `Halo nama saya ${nama}, saya ${umur} tahun.`
+// }
+
+// var username = '@diazfarindra'
+
+// function instagramURL(base) {
+//     var url = 'https://instagram.com/'
+//     return url + base
+// }
+
+// console.log(instagramURL(username));
