8f8f41163c031c140f47f6a2c1502317f2eeadc1
diaz farindra <farindrad@gmail.com>
2021-11-04 17:32:28 +0700

    update

---
+++ lanjutan/script8.x.js
@@ -0,0 +1,9 @@
+// Spread Operator
+
+const data = ['farindra', 'diaz', 'ibrahim']
+const data1 = ['farindra', 'diaz', 'ibrahim']
+console.log([...data, ...data1])
+
+// const flash_data = document.querySelectorAll('.flash-data')
+// const data = [...flash_data].map((flash_data) => flash_data.dataset.flashdata)
+// console.log(data)
