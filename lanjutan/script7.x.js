fa60b7ad49d447eff1650ab679f6560304aa60ec
diaz farindra <farindrad@gmail.com>
2021-11-04 17:05:35 +0700

    update

---
+++ lanjutan/script7.x.js
@@ -0,0 +1,31 @@
+//? for...of
+// const data = ['diaz', 'farindra', 'ibrahim']
+
+// for (const [i, d] of data.entries()) {
+//     console.log(i, d)
+// }
+
+// function total() {
+//     let total = 0;
+//     for (const iterator of arguments) {
+//         total += iterator
+//     }
+//     return total
+// }
+
+// console.log(total(1, 2, 3, 4, 5))
+
+
+//? for...in
+// const data = {
+//     nama: 'diaz farindra',
+//     umur: 18,
+//     email: 'farindrad@gmail.com'
+// }
+
+// for (const d in data) {
+//     if (Object.hasOwnProperty.call(data, d)) {
+//         const result = `${d} = ${data[d]}`;
+//         console.log(result);
+//     }
+// }
