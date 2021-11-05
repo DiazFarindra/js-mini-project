a8bf121db57953b2f0f3eb30cc4f563d495049ad
diaz farindra <farindrad@gmail.com>
2021-11-05 16:49:54 +0700

    update

--- lanjutan/script8.x.js
+++ lanjutan/script8.x.js
@@ -1,9 +1,32 @@
 // Spread Operator
+// const data = ['farindra', 'diaz', 'ibrahim']
+// const data1 = ['farindra', 'diaz', 'ibrahim']
+// console.log([...data, ...data1])
 
-const data = ['farindra', 'diaz', 'ibrahim']
-const data1 = ['farindra', 'diaz', 'ibrahim']
-console.log([...data, ...data1])
 
-// const flash_data = document.querySelectorAll('.flash-data')
-// const data = [...flash_data].map((flash_data) => flash_data.dataset.flashdata)
-// console.log(data)
+// Rest Parameter
+// function jumlahkan(...values) {
+//     return values.reduce((a, b) => a + b)
+// }
+// console.log(jumlahkan(1, 2, 3, 4, 5))
+
+// Array Destructuring
+// const data = ['diaz', 'haikal', 'zikri', 'farindra']
+// const [ketua, wakil, ...anggota] = data
+// console.log(anggota)
+
+// Object Destructuring
+// const team = {
+//     pm: 'diaz',
+//     fe: 'haikal',
+//     be: 'zikri',
+// }
+// const {pm, ...myteam} = team
+// console.log(myteam)
+
+// filtering
+function filterBy(type, ...values) {
+    return values.filter(v => typeof v === type)
+}
+
+console.log(filterBy('number', 1, 2, 'diaz', false, 10, true, 'farindra'))
